1dd30c6812335ff1ca1136af8018bf1b328347ee
Vanshsmann <61492182+Vanshsmann@users.noreply.github.com>
2024-02-02 15:57:22 -0600

    Update BLE_client_2characteristics.ino

--- BLE_client_2characteristics.ino
+++ BLE_client_2characteristics.ino
@@ -1,9 +1,3 @@
-/**
- * A BLE client example that is rich in capabilities.
- * There is a lot new capabilities implemented.
- * author unknown
- * updated by chegewara and MoThunderz
- */
 
 #include "BLEDevice.h"
 //#include "BLEScan.h"
