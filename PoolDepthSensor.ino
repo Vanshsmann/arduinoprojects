25336f2fafd9541ae9fa401c10412048fd360b09
Vanshsmann <61492182+Vanshsmann@users.noreply.github.com>
2024-02-02 15:56:43 -0600

    these 2 files for testing the sensor

---
+++ PoolDepthSensor.ino
@@ -0,0 +1,119 @@
+/*********
+  Rui Santos
+  Complete instructions at https://RandomNerdTutorials.com/esp32-ble-server-client/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <Wire.h>
+
+const int trigPin = 22;
+const int echoPin = 23;
+
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 1000;
+//BLE server name
+#define bleServerName "PoolDepthSensor"
+bool deviceConnected = false;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+#define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
+
+// Temperature Characteristic and Descriptor
+
+  BLECharacteristic bmeDepthCharacteristics("cba1d466-344c-4be3-ab3f-189f80dd7518", BLECharacteristic::PROPERTY_NOTIFY);
+  BLEDescriptor bmeDepthDescriptor(BLEUUID((uint16_t)0x2902));
+
+
+//Setup callbacks onConnect and onDisconnect
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  };
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
+
+
+
+void setup() {
+  // Start serial communication 
+  Serial.begin(115200); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  // Create the BLE Device
+  BLEDevice::init(bleServerName);
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *bmeService = pServer->createService(SERVICE_UUID);
+
+  // Create BLE Characteristics and Create a BLE Descriptor
+  // Temperature
+ 
+    bmeService->addCharacteristic(&bmeDepthCharacteristics);
+    bmeDepthDescriptor.setValue("BME temperature Celsius");
+    bmeDepthCharacteristics.addDescriptor(&bmeDepthDescriptor);
+
+
+  // Start the service
+  bmeService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+  if (deviceConnected) {
+    if ((millis() - lastTime) > timerDelay) {
+
+
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+      //Notify temperature reading from BME sensor
+        static char Depth[6];
+        dtostrf(distanceCm, 6, 2, Depth);
+        //Set temperature Characteristic value and notify connected client
+        bmeDepthCharacteristics.setValue(Depth);
+        bmeDepthCharacteristics.notify();
+        Serial.print("Depth: ");
+        Serial.print(distanceCm);
+        Serial.print(" Cm\n");
+
+      
+      lastTime = millis();
+    }
+  }
+}
